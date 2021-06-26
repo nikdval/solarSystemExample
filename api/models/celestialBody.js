feb86a59d78f6b0578187c07e72367077d930f17
Valia <valia.nikolakopoulou@controllis.com>
2021-06-26 13:40:23 +0100

    Celestial Body model

---
+++ api/models/celestialBody.js
@@ -0,0 +1,10 @@
+class CelestialBody {
+    constructor(name, picture, age, mass){
+        this.name = name;
+        this.picture = picture;
+        this.age = age;
+        this.mass = mass;
+    }
+}
+
+module.exports = CelestialBody;
